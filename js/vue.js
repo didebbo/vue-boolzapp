95332e8b8ee4d851bc6040d6896fc95e11b7c940
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-21 19:02:26 +0200

    aggiunta funzione risposta

--- js/vue.js
+++ js/vue.js
@@ -4,6 +4,109 @@ const app = new Vue(
         data: {
             currentContact: 0,
             currentMessage: "",
+            messages: [
+                "Sì",
+                "È così.",
+                "Sarebbe bello.",
+                "E come?",
+                "Giustamente.",
+                "Sta bene.",
+                "Si può anche convenire.",
+                "Da quanto è stato detto almeno, pare così.",
+                "Di' pure come.",
+                "Sia pure così.",
+                "Come no?",
+                "Molto bene.",
+                "Certamente.",
+                "Tutto questo è stato assolutamente chiarito a sufficienza.",
+                "Così almeno sembra.",
+                "Bene.",
+                "Questo non lo sapevo.",
+                "È verissimo quel che dici.",
+                "Ma è chiaro!",
+                "Perfetto.",
+                "Infatti è così.",
+                "Come dici?",
+                "Ma per forza!",
+                "Ma certamente!",
+                "E perché non dovrei?",
+                "Si disse proprio così.",
+                "Come potrebbe essere diversamente?",
+                "Senza meno.",
+                "Eh già!",
+                "Dillo pure!",
+                "Si trattava proprio di questo.",
+                "Non ho capito.",
+                "Pare.",
+                "Bisogna ammetterlo senza riserva.",
+                "Sta proprio così.",
+                "È vero.",
+                "È assolutamente così.",
+                "Questo almeno è verosimile.",
+                "Dici bene.",
+                "Esattamente.",
+                "C'è proprio modo che questo avvenga soprattutto per questa ragione.",
+                "Perché no?",
+                "Difficilmente, sicuro!",
+                "Non potrebbe affatto.",
+                "Sicuro!",
+                "Veramente no.",
+                "E come non ammetterlo?",
+                "Tu dici bene: bisogna fare così.",
+                "Va chiamata così.",
+                "Ebbene?",
+                "Giusto.",
+                "Come e a quale scopo dici questo?",
+                "E perché?",
+                "È impossibile.",
+                "Assolutamente no.",
+                "È assolutamente necessario.",
+                "In che senso dici?",
+                "È molto chiaro.",
+                "Sì, nel modo più assoluto.",
+                "Ti seguo.",
+                "Pare certamente così.",
+                "Lo vedo anche troppo bene.",
+                "E che cos'altro potremmo dire?",
+                "Anche questo.",
+                "Mi pare che questo punto si debba dibattere nei nostri ragionamenti.",
+                "È verissimo.",
+                "Lo hai detto.",
+                "Per quel che mi riguarda non mi sembra affatto che tu sbagli.",
+                "Di' pure chiaro quel che vuoi dire.",
+                "Lo vedo.",
+                "Forse sì.",
+                "Eh, sì!",
+                "Tu dici il vero.",
+                "È molto giusto quello che dici.",
+                "È necessario.",
+                "Sembra senza dubbio che la questione stia così.",
+                "Sarà così.",
+                "Tutto questo a me pare che sia assurdo!",
+                "Certamente sì.",
+                "Verissimo.",
+                "Senza dubbio.",
+                "Assolutamente.",
+                "È proprio così.",
+                "È come dici.",
+                "Tu dici cosa autentica e vera.",
+                "Questo anch'io mi sento capace di affermarlo.",
+                "Sì, certamente.",
+                "Forse.",
+                "È esatto.",
+                "Ma questo è impossibile.",
+                "Pressappoco.",
+                "Ma certo.",
+                "Senza remora alcuna, assolutamente.",
+                "Nel modo più evidente.",
+                "Su per giù è così.",
+                "Dici il giusto.",
+                "È giustissimo.",
+                "A questo punto non ci sono arrivato.",
+                "È verosimile.",
+                "Tutto il contrario."
+            ],
+
             contacts: [
                 {
                     name: 'Michele',
@@ -97,12 +200,24 @@ const app = new Vue(
             sendMessage: function () {
                 this.contacts[this.currentContact].messages.push(
                     {
-                        date: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString('en-US', { hour12: false }),
+                        date: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString('us-US', { hour12: false }),
                         message: this.currentMessage,
                         status: 'sent'
                     }
                 );
                 this.currentMessage = "";
+                this.replayMessage();
+            },
+            replayMessage: function () {
+                setTimeout(() => {
+                    this.contacts[this.currentContact].messages.push(
+                        {
+                            date: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString('us-US', { hour12: false }),
+                            message: this.messages[Math.floor(Math.random() * this.messages.length)],
+                            status: 'received'
+                        }
+                    );
+                }, 1000 * Math.floor(Math.random() * 10 + 1));
             }
         }
     }
